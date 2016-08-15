bcddc970f769918f09877d119d861291e1e8572d
tonlen <1750041@qq.com>
2016-08-15 18:03:34 +0800

    Update TPPopoverBackgroundView.m

--- PopoverView/Class/TPPopoverBackgroundView.m
+++ PopoverView/Class/TPPopoverBackgroundView.m
@@ -2,8 +2,8 @@
 //  PopoverBackgroundView.m
 //  CustomPopover
 //
-//  Created by Aaron Crabtree on 3/25/13.
-//  Copyright (c) 2013 Tap Dezign. All rights reserved.
+//  Created by Aaron Crabtree on 16/3/25.
+//  Copyright (c) 2016 len. All rights reserved.
 //
 
 #import "TPPopoverBackgroundView.h"
